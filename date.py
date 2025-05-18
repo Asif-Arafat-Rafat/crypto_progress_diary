6252d110374da2ecd84cb80f1f409d4d5bf47493
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-05-19 00:55:31 +0600

    Fixed to avoid date repeatation

--- date.py
+++ date.py
@@ -4,34 +4,32 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 def file_name(coins):
     today = date.today()
-    # print("Today's date:", today)
-    filename = f"TradingData.xlsx"
-    # filename = "trial.xlsx"
-    current_dir=os.path.dirname(os.path.abspath(__file__))
-    path=os.path.join(current_dir,"sub")
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if not os.path.exists(os.path.join(path,filename)):
-        wb=Workbook()
-        ws=wb.active
-        ws.append(["Coin Name"])
-        ws.title="date=="+str(today)
-        wb.save(os.path.join(path,filename))
-    wb=load_workbook(os.path.join(path,filename))
+    filename="TradingData.xlsx"
+    if not os.path.exists("sub"):
+        os.makedirs("sub")
+    if os.path.exists(f"{os.path.abspath(__file__)}/sub/{filename}"):
+        wb=load_workbook(f"{os.path.abspath(__file__)}/sub/{filename}")
+    else:
+        wb=Workbook( )
     ws=wb.active
-    found=False
-    mxc=ws.max_column
-    ws[get_column_letter(mxc+1)+str(1)]=str(today)
-    for key,value in coins.items():
-        for i in range(1,ws.max_row+1):
-            if(key==ws["A"+str(i)].value):
-                ws[get_column_letter(mxc)+str(i)]=value
-                found=True
-                break
-        if not found:
-            ws["A"+str(ws.max_row+1)]=key
-            ws[get_column_letter( mxc+1)+str( ws.max_row)]=value
-        else:
-            found=False
 
-    wb.save(os.path.join(path,filename))
+    if ws.cell(row=1,column=ws.max_column).value==str(today):
+        pos=ws.max_column
+        ws.delete_cols(pos)
+    else:
+        pos=ws.max_column+1
+    ws.cell(row=1,column=pos).value=str(today)    
+    ws.title="Trading Data"
+    ws["A1"]="Coin"
+    for key,value in coins.items():
+        found=False
+        for i in range(1,ws.max_row):
+            if ws.cell(row=i,column=1).value==key:
+                ws.cell(row=i,column=pos).value=value
+                found=True
+        if not found:
+            pr=ws.max_row+1
+            ws.cell(row=pr,column=1).value=key
+            ws.cell(row=pr,column=pos).value=value
+
+    wb.save(f"sub/{filename}") 
